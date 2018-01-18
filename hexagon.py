5ed5db632d7fed1023c3ab1dbb214d06dd277930
Huyen <nghforapp@gmail.com>
2018-01-18 23:39:49 +0700

    Update

--- hexagon.py
+++ hexagon.py
@@ -5,7 +5,8 @@ length = 15
 upper = " / \\"
 side = "|   "
 lower = " \\ /"
-hexg = [10,10]
+hexg = [11,11]
+
 
 def draw_y_axis():
     print "0\t|" + "_" * 170  # FIXXED x-axis
@@ -28,7 +29,6 @@ def draw_hexagon_with_axis():  # -----------------------------------------------
             print (str(length-i) + "\t|\t  " + side * (width + 1) + "\n\t|\t /" + lower * width)  # print also y-axis
         else:
             print (str(length-i) + "\t|\t" + side * (width + 1) + "\n\t|\t" + lower * width + " \\")  # print y-axis
-
     # print "0\t|" + "_"*5*width          # SOFT x-axis
     draw_y_axis()
 
@@ -66,8 +66,31 @@ def color_single_hexagon():  # ----------------------COLOR----------------------
 
     draw_y_axis()
 
+
 def color_hexagons():
-    # ----- READ from file, PUT into list of list  ------
+    # ----- Sub functions ------
+    def prep_even():
+        screen[i] += side * (width + 1) + "\n\t|\t"
+        screen[i] += lower * hexa[1]
+        for j in range(2, len(hexa)):
+            screen[i] += "*\\ /" + lower * (hexa[j] - hexa[j - 1] - 1)
+        screen[i] += "*\\"
+        if hexa[-1] < width:
+            screen[i] += " /" + lower * (width - hexa[-1] - 1) + " \\"
+
+    def main_side():
+        full[i] += side * (hexa[1] - 1) + "|***"
+        for j in range(2, len(hexa)):
+            full[i] += side * (hexa[j] - hexa[j - 1] - 1) + "|***"
+        full[i] += side * (width - hexa[-1] + 1)
+
+    def main_lower():
+        full[i] += lower * (hexa[1] - 1) + " \\*/"
+        for j in range(2, len(hexa)):
+            full[i] += lower * (hexa[j] - hexa[j - 1] - 1) + " \\*/"
+        full[i] += lower * (width - hexa[-1])
+
+    # --------- READ from file, PUT into list of list  -----------
     with open("data.txt") as f:
         lis = []
         for line in f:
@@ -75,7 +98,7 @@ def color_hexagons():
 
     sorted_list = sorted(lis, key=lambda x: x[0])  # Sort
 
-    grouped_list = []                                         # ----- GROUP ----
+    grouped_list = []           # ----- GROUP ----
     for k in range(length + 1):
         m = [k]
         for e in sorted_list:
@@ -84,11 +107,10 @@ def color_hexagons():
         grouped_list.append(m)
 
     grouped_list = [x for x in grouped_list if len(x) > 1]
-    print grouped_list
-
+    # print grouped_list
     screen = []
     full = []
-    # ----------------------- COLORING --------------------------
+    # ----------------------- COLORING -------------------------
     for i in range(length):
         screen.append("")
         full.append("")
@@ -96,23 +118,19 @@ def color_hexagons():
         full[i] = ""
         prep = 0
         m = 0  # main
+
+    # ---------------------- y-axis ----------------------------
         if i % 2:
             print str(length - i) + "\t|\t ",
         elif i != 0:
             print str(length - i) + "\t|\t",
-
+    # ----------------------- SELECT ---------------------------
         for hexa in grouped_list:
             if i == length - hexa[0] - 1:  # ----- PREPARE -----
                 prep = 1
                 if i == 0:
-                    screen[i] += "\t|\t" + upper * width
-                    screen[i] += "\n" + str(length - i) + "\t|\t" + side * (width + 1) + "\n\t|\t"
-                    screen[i] += lower * hexa[1]
-                    for j in range(2, len(hexa)):
-                        screen[i] += "*\\ /" + lower * (hexa[j] - hexa[j - 1] - 1)
-                    screen[i] += "*\\"
-                    if hexa[-1] != width:
-                        screen[i] += " /" + lower * (width - hexa[-1] - 1) + " \\"
+                    screen[i] += "\t|\t" + upper * width + "\n" + str(length - i) + "\t|\t"
+                    prep_even()
 
                 elif i % 2:
                     screen[i] += side * (width + 1) + "\n\t|\t /"
@@ -120,15 +138,8 @@ def color_hexagons():
                     for j in range(2, len(hexa)):
                         screen[i] += lower * (hexa[j] - hexa[j - 1] - 1) + "*\\ /"
                     screen[i] += lower * (width - hexa[-1])
-
                 else:
-                    screen[i] += side * (width + 1) + "\n\t|\t"
-                    screen[i] += lower * hexa[1]
-                    for j in range(2, len(hexa)):
-                        screen[i] += "*\\ /" + lower * (hexa[j] - hexa[j - 1] - 1)
-                    screen[i] += "*\\"
-                    if hexa[-1] < width:
-                        screen[i] += " /" + lower * (width - hexa[-1] - 1) + " \\"
+                    prep_even()
 
             if i == length - hexa[0]:  # --------- MAIN ----------
                 m = 1
@@ -138,61 +149,36 @@ def color_hexagons():
                         full[i] += upper * (hexa[j] - hexa[j - 1] - 1) + " /*\\"
                     full[i] += upper * (width - hexa[-1])
 
-                    # -----------
-                    full[i] += "\n" + str(length - i) + "\t|\t" + side * (hexa[1] - 1) + "|***"
-                    for j in range(2, len(hexa)):
-                        full[i] += side * (hexa[j] - hexa[j - 1] - 1) + "|***"
-                    full[i] += side * (width - hexa[-1] + 1)
-
-                    # -----------
-
-                    full[i] += "\n\t|\t" + lower * (hexa[1] - 1) + " \\*/"
-                    for j in range(2, len(hexa)):
-                        full[i] += lower * (hexa[j] - hexa[j - 1] - 1) + " \\*/"
-                    full[i] += lower * (width - hexa[-1]) + " \\"
+                    full[i] += "\n" + str(length - i) + "\t|\t"
+                    main_side()
+                    full[i] += "\n\t|\t"
+                    main_lower()
 
                 elif i % 2:
-                    # Side
-                    full[i] += side * (hexa[1] - 1) + "|***"
-                    for j in range(2, len(hexa)):
-                        full[i] += side * (hexa[j] - hexa[j - 1] - 1) + "|***"
-                    full[i] += side * (width - hexa[-1] + 1)
-
-                    # Lower
-                    full[i] += "\n\t|\t /" + lower * (hexa[1] - 1) + " \\*/"
-                    for j in range(2, len(hexa)):
-                        full[i] += lower * (hexa[j] - hexa[j - 1] - 1) + " \\*/"
-                    full[i] += lower * (width - hexa[-1])
+                    main_side()
+                    full[i] += "\n\t|\t /"
+                    main_lower()
 
                 else:
-                    full[i] += side * (hexa[1] - 1) + "|***"
-                    for j in range(2, len(hexa)):
-                        full[i] += side * (hexa[j] - hexa[j - 1] - 1) + "|***"
-                    full[i] += side * (width - hexa[-1] + 1)
-
-                    full[i] += "\n\t|\t" + lower * (hexa[1] - 1) + " \\*/"
-                    for j in range(2, len(hexa)):
-                        full[i] += lower * (hexa[j] - hexa[j - 1] - 1) + " \\*/"
-                    full[i] += lower * (width - hexa[-1]) + " \\"
+                    main_side()
+                    full[i] += "\n\t|\t"
+                    main_lower()
+                    full[i] += " \\"
 
             # ----- PRINT ------
             if hexa == grouped_list[-1]:
                 if prep and m:
                     print ''.join(map(max, screen[i], full[i]))
-
                 elif prep and (m == 0):
                     print screen[i]
-
                 elif m and prep == 0:
                     print full[i]
 
                 elif prep == 0 and m == 0:
                     if i == 0:
                         print ("\t|\t" + upper * width + "\n" + str(length - i) + "\t|\t" + side * (width + 1) + "\n\t|\t" + lower * width + " \\")
-
                     elif i % 2:
                         print side * (width + 1) + "\n\t|\t /" + lower * width
-
                     else:
                         print side * (width + 1) + "\n\t|\t" + lower * width + " \\"
     draw_y_axis()
@@ -201,4 +187,3 @@ color_hexagons()
 # draw_hexagon()
 # draw_hexagon_with_axis()
 # color_single_hexagon()
-
